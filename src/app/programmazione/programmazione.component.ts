fcd2148acb541d803d562dbfb087efb2ceadaeb1
AdelmoTroiani93 <36970861+AdelmoTroiani93@users.noreply.github.com>
2025-03-25 16:38:26 +0100

    new modify onDestroy

--- src/app/programmazione/programmazione.component.ts
+++ src/app/programmazione/programmazione.component.ts
@@ -1,26 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ProgrammazioneService } from './services/programmazione.service';
 import { ProiezioneDTO } from '../dto/proiezione.dto';
 import { PagedResponse } from '../dto/paged-response-proiezione.dto';
-
+import { Subject, takeUntil } from 'rxjs';
 @Component({
   selector: 'app-programmazione',
   standalone: false,
   templateUrl: './programmazione.component.html',
   styleUrl: './programmazione.component.css',
 })
-export class ProgrammazioneComponent implements OnInit {
+export class ProgrammazioneComponent implements OnInit, OnDestroy {
   proiezioni!: ProiezioneDTO[];
   currentPage: number = 0;
   pageMax: number = 0;
   pageArray: any;
   dataProiezione: any = '';
   dataFine: any = '';
+  private destroy$ = new Subject<void>();
   constructor(private programmazioneService: ProgrammazioneService) {}
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
   ngOnInit(): void {
     this.programmazioneService
       .getAllProiezione(this.currentPage, this.dataProiezione, this.dataFine)
+      .pipe(takeUntil(this.destroy$))
       .subscribe((dataProiezioni: PagedResponse) => {
         this.pageMax = dataProiezioni.totalPages;
         this.pageArray = Array.from({ length: this.pageMax }, (_, i) => i);
--- src/app/storico/storico.component.ts
+++ src/app/storico/storico.component.ts
@@ -1,29 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ProiezioneDTO } from '../dto/proiezione.dto';
 import { ProgrammazioneService } from '../programmazione/services/programmazione.service';
 
 import { StoricoDTO } from '../dto/storico.dto';
 import { PagedResponse } from '../dto/paged-response-storico.dto';
 import { StoricoService } from './services/storico.service';
-
+import { Subject, takeUntil } from 'rxjs';
 @Component({
   selector: 'app-storico',
   standalone: false,
   templateUrl: './storico.component.html',
   styleUrl: './storico.component.css',
 })
-export class StoricoComponent {
+export class StoricoComponent implements OnInit, OnDestroy {
   storico!: StoricoDTO[];
   currentPage: number = 0;
   pageMax: number = 0;
   pageArray: any;
   dataInizio: any = '';
   dataFine: any = '';
+  private destroy$ = new Subject<void>();
   constructor(private storicoService: StoricoService) {}
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
   ngOnInit(): void {
     this.storicoService
       .getAllStorico(this.currentPage, this.dataInizio, this.dataFine)
+      .pipe(takeUntil(this.destroy$))
       .subscribe((dataProiezioni: PagedResponse) => {
         this.pageMax = dataProiezioni.totalPages;
         this.pageArray = Array.from({ length: this.pageMax }, (_, i) => i);
